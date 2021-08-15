3587a36d3c724712e313d0d09aed2a226cc45545
Christoph Beyer <the-real-christoph>
2021-08-15 23:17:26 +0200

    Add failed job retry without artisan usage to support missing class files too.

--- src/Http/Controllers/FailedJobsController.php
+++ src/Http/Controllers/FailedJobsController.php
@@ -4,6 +4,7 @@ namespace LumenQueueManager\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Routing\Controller;
 use LumenQueueManager\Models\FailedJob;
@@ -19,8 +20,7 @@ class FailedJobsController extends Controller
 
         $queueInfos = FailedJob::select('queue', DB::raw('count(queue) as numberOfJobs'))->groupBy('queue')->get()->toArray();
         $queueSelectionOptions = ['default' => 'default'];
-        foreach($queueInfos as $queueInfo)
-        {
+        foreach ($queueInfos as $queueInfo) {
             $queueSelectionOptions[$queueInfo['queue']] = $queueInfo['queue'] . ' (' . $queueInfo['numberOfJobs'] . ' items)';
         }
 
@@ -28,9 +28,8 @@ class FailedJobsController extends Controller
 
         /** @var LengthAwarePaginator $jobs */
         $jobs = FailedJob::where('queue', $currentQueue)->paginate(config('lumen-queue-manager.itemsPerPage', 10));
-        if($jobs->count() == 0 && $jobs->total() > 0)
-        {
-            $newPage = (int)(($jobs->total()-1) / $jobs->perPage()) + 1;
+        if ($jobs->count() == 0 && $jobs->total() > 0) {
+            $newPage = (int)(($jobs->total() - 1) / $jobs->perPage()) + 1;
             return redirect()->route('queue-manager-failed-index', ['page' => $newPage] + $request->all());
         }
 
@@ -43,14 +42,14 @@ class FailedJobsController extends Controller
         ]);
     }
 
-    public function view(Request $request, $jobId) {
+    public function view(Request $request, $jobId)
+    {
         $this->checkForDatabaseQueue();
         $currentQueue = $request->input('queue', 'default');
 
         /** @var FailedJob $job */
         $job = FailedJob::whereId($jobId)->first();
-        if(null === $job)
-        {
+        if (null === $job) {
             return redirect()->route('queue-manager-index', [
                 'queue' => $request->input('queue', 'default'),
                 'message' => "Job cannot be found, maybe it has been deleted or already worked away."
@@ -84,16 +83,26 @@ class FailedJobsController extends Controller
 
         /** @var FailedJob $failedJob */
         $failedJob = FailedJob::whereUuid($jobUuid)->first();
-        if(null === $failedJob)
-        {
+        if (null === $failedJob) {
             return redirect()->route('queue-manager-failed-index', [
                 'queue' => $currentQueue,
                 'message' => 'Job cannot be found!'
             ]);
         }
 
+        $infoMessage = $this->retryUsingManualWay($failedJob); // deactivate once illuminate got patched
+        // activate once illuminate got patched $this->retryUsingArtisan($jobUuid);
+
+        return redirect()->route('queue-manager-failed-index', [
+            'queue' => $currentQueue,
+            'message' => $infoMessage ?? "Job has been rescheduled."
+        ]);
+    }
+
+    protected function retryUsingManualWay(FailedJob $failedJob)
+    {
         $jobData = json_decode($failedJob->payload, true);
-        if(isset($jobData['retryUntil'])) {
+        if (isset($jobData['retryUntil'])) {
             $jobData['retryUntil'] = null;
             $infoMessage = "Job had retryUntil attribute, this had to be removed when rescheduling.";
         }
@@ -107,17 +116,21 @@ class FailedJobsController extends Controller
         ]);
         $failedJob->delete();
 
-        return redirect()->route('queue-manager-failed-index', [
-            'queue' => $currentQueue,
-            'message' => $infoMessage ?? "Job has been rescheduled."
-        ]);
+        return $infoMessage ?? null;
+    }
+
+    protected function retryUsingArtisan($jobUuid)
+    {
+        $outputBuffer = null;
+        $result = Artisan::call('queue:retry', ['id' => $jobUuid], $outputBuffer);
+        return Artisan::output();
     }
 
     protected function checkForDatabaseQueue()
     {
         // register Queue component to load related config
         app('queue');
-        if(config('queue.default') !== 'database') {
+        if (config('queue.default') !== 'database') {
             throw new NotFoundHttpException('Only installations with database queue supported');
         }
     }
